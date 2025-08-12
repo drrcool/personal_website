be84beac2c4b0f67ebc8d02d3bd5d264a9ce3388
Richard Cool <rcool@netflix.com>
2025-08-12 09:15:40 -0700

    Update google analytics

--- src/components/analytics/google-analytics.tsx
+++ src/components/analytics/google-analytics.tsx
@@ -13,17 +13,15 @@ export default function GoogleAnalytics() {
     <>
       <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-        strategy="afterInteractive"
       />
-      <Script id="google-analytics" strategy="afterInteractive">
+      <Script id="google-analytics">
         {`
+          console.log("GA: Initializing analytics...");
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '${GA_MEASUREMENT_ID}', {
-            page_title: document.title,
-            page_location: window.location.href,
-          });
+          gtag('config', '${GA_MEASUREMENT_ID}');
+          console.log("GA: Analytics initialized");
         `}
       </Script>
     </>
