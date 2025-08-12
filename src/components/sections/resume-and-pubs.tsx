d0e99063dd805cd9c3b09fb66fe8c7bfecdfdae5
Richard Diaz-Cool <rcool@netflix.com>
2025-08-11 21:09:24 -0700

    Correct name of section

--- src/components/sections/resume-and-pubs.tsx
+++ src/components/sections/resume-and-pubs.tsx
@@ -4,7 +4,7 @@ import ResumeButton from "./resume-button";
 
 const ResumeAndPublicationsSection = () => {
   return (
-    <SectionComponent label="Resume and Publications">
+    <SectionComponent label="Resume & Publications">
       <div className="flex flex-column md:flex-row gap-2 my-8 justify-center items-center">
         <ResumeButton />
       </div>
