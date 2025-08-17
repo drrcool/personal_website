f5f195d1225d6c2ff39be9f060cef4237c4cd867
Richard Cool <rcool@netflix.com>
2025-08-17 11:19:44 -0700

    Added landing page for the helpline views

---
+++ src/app/helpline/page.tsx
@@ -0,0 +1,9 @@
+export default function Home(): React.ReactElement {
+  return (
+    <>
+      <div className="flex flex-col md:flex-row gap-0">
+        Dashboard will go here
+      </div>
+    </>
+  );
+}
