05ec4ac4e879455dd13db21eb81b8215e2286aaa
Richard Cool <rcool@netflix.com>
2025-08-17 12:28:21 -0700

    Add layout for helpline after removing mui

--- src/app/helpline/layout.tsx
+++ src/app/helpline/layout.tsx
@@ -1,12 +1,14 @@
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
+import HelplineHeader from "@/components/helpline-components/layout/helpline-header";
 
 export default function HelplineLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>): React.ReactElement {
-  return <div>{children}</div>;
+  return (
+    <div className="flex flex-col gap-0 w-full">
+      <HelplineHeader />
+      <div className="block m-5 w-full h-full">{children}</div>
+    </div>
+  );
 }
