7668dfe762e69e3e283c615dff327ba2010c6ef1
Richard Cool <rcool@netflix.com>
2025-08-17 20:38:56 -0700

    Set minimal inputs

--- src/components/helpline-components/dashboard-views/calls/call-time-series.tsx
+++ src/components/helpline-components/dashboard-views/calls/call-time-series.tsx
@@ -1,12 +1,17 @@
 import { Card } from "@/components/ui/card";
 
+import StackedArea from "../../charts/stacked-area";
 import { getCallTimeSeries } from "../../dataFetchers/getCallTimeSeries";
 
 const CallTimeSeries = async () => {
   const data = await getCallTimeSeries({ helplineId: "GSC" });
   return (
-    <Card className="border-b">
-      <div>CallTimeSeries</div>
+    <Card className="p-4 text-card">
+      <StackedArea
+        data={data as unknown as Array<Record<string, number>>}
+        xKey="dateint"
+        yKey={["call_cnt", "missed_call_cnt"]}
+      />
     </Card>
   );
 };
