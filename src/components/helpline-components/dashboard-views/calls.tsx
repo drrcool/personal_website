ddecd194579015703f34fbb21034c89e57f1dbf1
Richard Cool <rcool@netflix.com>
2025-08-17 19:33:40 -0700

    Add styling

--- src/components/helpline-components/dashboard-views/calls.tsx
+++ src/components/helpline-components/dashboard-views/calls.tsx
@@ -2,9 +2,11 @@ import CallTimeSeries from "./calls/call-time-series";
 
 const Calls = () => {
   return (
-    <div className="m-4 flex flex-col gap-5">
+    <div className="m-4 flex flex-col gap-5 w-full">
       <div>Calls</div>
-      <CallTimeSeries />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 h-[600px]">
+        <CallTimeSeries />
+      </div>
     </div>
   );
 };
